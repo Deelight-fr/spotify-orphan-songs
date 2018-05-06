119867073df3590f832ebdc2658d632e89c533cb
Deelight <deelight@logeek.com>
2018-05-06 10:13:23 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spotify-orphan-songs
 Create a playlist from songs in the library, but not included in any playlist
+
+Depencies:
+pip3 install spotipy
---
+++ spotify-orphan-songs.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import sys
+import spotipy
+import spotipy.util as util
+from datetime import datetime
+
+sp = spotipy.Spotify()
+
+scope = 'user-library-read playlist-modify-public'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+def show_tracks(results):
+    for i, item in enumerate(results['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+
+    #
+    # Playlist songs
+    #
+
+    playlist_songs = set()
+
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:
+        if playlist['owner']['id'] == username:
+            if not playlist['name'].startswith('Orphan songs '):
+                print("Scanning", playlist['name'], "-", playlist['tracks']['total'], "songs...")
+                results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+                tracks = results['tracks']
+                for item in tracks['items']:
+                    track = item['track']
+                    playlist_songs.add(track['uri'])
+
+                    # More pages ?
+                    while(tracks['next']):
+                        tracks = sp.next(tracks)
+                        for item in tracks['items']:
+                            track = item['track']
+                            playlist_songs.add(track['uri'])
+
+
+    print("Unique songs in playlists:", len(playlist_songs))
+
+    #
+    # Library songs
+    #
+
+    library_songs = set()
+
+    tracks = sp.current_user_saved_tracks(limit=50)
+    print("Scanning library -", tracks['total'], "songs...")
+
+    for item in tracks['items']:
+        track = item['track']
+        library_songs.add(track['uri'])
+        while(tracks['next']):
+            tracks = sp.next(tracks)
+            for item in tracks['items']:
+                track = item['track']
+                library_songs.add(track['uri'])
+
+    #
+    # Difference
+    #
+
+    orphan_songs = library_songs.difference(playlist_songs)
+
+    #
+    # New playlist
+    #
+
+    playlist_name = 'Orphan songs ' + datetime.today().isoformat()[:10]
+    print("Creating playlist:", playlist_name)
+    playlist = sp.user_playlist_create(username, playlist_name, public=True)
+
+    #
+    # Add orphans to playlist
+    #
+
+    print("Adding tracks to new playlist")
+    orphan_songs_list = list(orphan_songs)
+    for i in range(0, len(orphan_songs_list), 100):
+        chunk = orphan_songs_list[i:i + 100]
+        results = sp.user_playlist_add_tracks(username, playlist['id'], chunk)
+
+else:
+    print("Can't get token for", username)
